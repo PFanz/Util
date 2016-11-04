0d75c342fdf3e019c4c2dc779fc302ab5a672df8
彭飞凡 <pengfeifan@huanqiu.com>
2016-11-04 17:41:07 +0800

    修改一些兼容问题，等待验证

--- gulp&webpack/gulpfile.js
+++ gulp&webpack/gulpfile.js
@@ -10,8 +10,6 @@ var autoprefixer = require('gulp-autoprefixer')
 var cleanCSS = require('gulp-clean-css')
 var postcss = require('gulp-postcss')
 var webpack = require('webpack-stream')
-// var babel = require('gulp-babel')
-// var uglify = require('gulp-uglify')
 var imagemin = require('gulp-imagemin')
 var browserSync = require('browser-sync').create()
 
@@ -21,13 +19,20 @@ var errorHandler = (e) => {
   gutil.log(e)
 }
 
+gulp.task('jsTask', function () {
+  gulp.src('src/js/*.js')
+    .pipe(plumber({errorHandler: errorHandler}))
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('dist/js/'))
+})
+
 // postcss 配置
 var processors = [
   require('cssgrace')
 ]
 
 // css
-gulp.task('sass', function () {
+gulp.task('sassTask', function () {
   gulp.src('src/sass/*.scss')
     .pipe(plumber({errorHandler: errorHandler}))
     .pipe(sourcemaps.init())
@@ -44,46 +49,36 @@ gulp.task('sass', function () {
     .pipe(browserSync.reload({stream: true}))
 })
 
-// 压缩JS
-gulp.task('jsMin', function () {
+// 移动libs
+gulp.task('libsTask', function () {
   // jslib 直接移动
   gulp.src('src/libs/**')
     .pipe(gulp.dest('dist/libs'))
-  // js
-  gulp.src('src/js/*.js')
-    .pipe(plumber({errorHandler: errorHandler}))
-    // .pipe(sourcemaps.init())
-    // .pipe(babel())
-    // 使用webpack
-    .pipe(webpack(require('./webpack.config.js')))
-    // .pipe(uglify())
-    // .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/js/'))
 })
 
 // 压缩图片
-gulp.task('imageMin', function () {
+gulp.task('imgTask', function () {
   gulp.src('src/images/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest('dist/images/'))
 })
 
 // html
-gulp.task('releaseHtml', function () {
+gulp.task('htmlTask', function () {
   gulp.src('./*.html')
     .pipe(replace(/"dist\//g, releaseUrl))
     .pipe(gulp.dest('dist/'))
 })
 
 // browser-sync服务
-gulp.task('serve', ['sass', 'jsMin', 'imageMin'], function () {
+gulp.task('serve', ['imgTask', 'libsTask', 'sassTask', 'jsTask', 'htmlTask'], function () {
   browserSync.init({
     server: './'
   })
-  gulp.watch('src/sass/*.scss', ['sass'])
-  gulp.watch('src/js/*.js', ['jsMin']).on('change', browserSync.reload)
-  gulp.watch('src/images/*', ['imageMin']).on('change', browserSync.reload)
-  gulp.watch('*.html', ['jsMin', 'sass', 'imageMin', 'releaseHtml', 'serve']).on('change', browserSync.reload)
+  gulp.watch('src/sass/*.scss', ['sassTask'])
+  gulp.watch('src/js/*.js', ['jsTask']).on('change', browserSync.reload)
+  gulp.watch('src/images/*', ['imgTask']).on('change', browserSync.reload)
+  gulp.watch('*.html', ['htmlTask']).on('change', browserSync.reload)
 })
 
-gulp.task('default', ['jsMin', 'sass', 'imageMin', 'releaseHtml', 'serve'])
+gulp.task('default', ['serve'])
